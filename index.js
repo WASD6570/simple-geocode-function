2836b9d84faee4161fe0c8119439abf33c46ab4d
WASD6570 <wasd12.ns@gmail.com>
2022-02-25 14:48:04 -0300

    added maps

--- index.js
+++ index.js
@@ -1,52 +1,59 @@
+import distanceCalculator from "./distanceCalculator.js";
 import jsonData from "./test.json" assert { type: "json" };
 
-//funciones helper
+let map;
+let markers = [];
 
-//// funcion para pasar de grados a radianes
-
-function deg2rad(deg) {
-  return deg * (Math.PI / 180);
-}
-
-//// funcion para obtener la distancia entre 2 puntos
-
-function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-  const R = 6371; // Radius of the earth in km
-  const dLat = deg2rad(lat2 - lat1);
-  const dLon = deg2rad(lon2 - lon1);
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) *
-      Math.cos(deg2rad(lat2)) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = R * c; // Distance in km
-  return distance;
-}
-
-//funcion main
-
-export default function main(lat_actual, lng_actual, radius, jsonComercios) {
-  const { comercios } = jsonComercios;
-  const raw_array_of_comercios = comercios.map((comercio) => {
-    if (
-      getDistanceFromLatLonInKm(
-        lat_actual,
-        lng_actual,
-        comercio.lat,
-        comercio.lng
-      ) <= radius
-    ) {
-      comercio.isOnRadius = true;
+window.initMap = function () {
+  // JS API is loaded and available
+  navigator.geolocation.getCurrentPosition(
+    async (location) => {
+      localStorage.setItem(
+        "userLocation",
+        JSON.stringify({
+          lat: location.coords.latitude,
+          lng: location.coords.longitude,
+        })
+      );
+    },
+    (error) => {
+      window.alert("no pudimos obtener tu ubicacion");
     }
-    if (comercio.isOnRadius) {
-      return comercio;
-    }
+  );
+  const localData = localStorage.getItem("userLocation");
+  const userLocation = localData
+    ? JSON.parse(localData)
+    : { lat: -30, lng: -60 };
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: userLocation.lat,
+      lng: userLocation.lng,
+    },
+    zoom: 8,
   });
-  // limpia el array de valores falsy
-  return raw_array_of_comercios.filter((item) => item);
-}
-////// console log de pruebas, los comercios estan a 0.066km y 1.77km
+  createMarker({
+    lat: userLocation.lat,
+    lng: userLocation.lng,
+  });
 
-// console.log(main(-30.632211, -67.471279, 7, jsonData));
+  distanceCalculator(-30.632211, -67.471279, 1, jsonData);
+};
+
+function createMarker(latLngObject) {
+  const marker = new google.maps.Marker({
+    position: {
+      lat: latLngObject.lat,
+      lng: latLngObject.lng,
+    },
+    map: map,
+  });
+  markers.push(marker);
+}
+
+function removeMarkers() {
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
+  return;
+}
